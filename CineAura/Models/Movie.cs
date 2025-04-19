bbff025e02f841b9f57a77c5e0f772b49848004f
ErinaTernava <et70055@ubt-uni.net>
2025-04-20 01:04:40 +0200

    movie model

--- CineAura/Models/Movie.cs
+++ CineAura/Models/Movie.cs
@@ -1,6 +1,20 @@
-﻿namespace CineAura.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineAura.Models
 {
     public class Movie
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte[]? Photo { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
     }
 }
