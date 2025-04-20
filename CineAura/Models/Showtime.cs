7761bfabcc5177c867194058172b7f1c5f7cd7f5
ErinaTernava <et70055@ubt-uni.net>
2025-04-20 18:55:36 +0200

    Showtime model

---
+++ CineAura/Models/Showtime.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineAura.Models
+{
+    public class Showtime
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        [Required]
+        public int HallId { get; set; }
+        public Hall Hall { get; set; }
+        public DateTime StartTime { get; set; }
+        public decimal TicketPrice { get; set; }
+
+    }
+}
