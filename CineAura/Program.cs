fe5d25e42967c0fd7bec919ea43f0a48f0e98b48
ErinaTernava <et70055@ubt-uni.net>
2025-04-25 21:22:34 +0200

    Service scope extender class

--- CineAura/Program.cs
+++ CineAura/Program.cs
@@ -10,12 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IHallService, HallService>();
-builder.Services.AddScoped<ISeatService, SeatService>();
-builder.Services.AddScoped<IMovieService, MovieService>();
-builder.Services.AddScoped<ICartService, CartService>();
-
+builder.Services.AddScopedServices();
 
 // Add services to the container.
 builder.Services.AddControllers();
---
+++ CineAura/Services/ServiceCollectionExtentions.cs
@@ -0,0 +1,17 @@
+﻿using CineAura.Services.Interfaces;
+using System.Runtime.CompilerServices;
+
+namespace CineAura.Services
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static void AddScopedServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IHallService, HallService>();
+            services.AddScoped<ISeatService, SeatService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<ICartService, CartService>();
+        }
+    }
+}
