826338e73f1f2fbf8e96a4aee4538d415059a49c
Vesa-Sopjani <vs70037@ubt-uni.net>
2025-05-08 22:36:56 +0200

    added new methods in hall controller for crud

--- CineAura/Controllers/HallController.cs
+++ CineAura/Controllers/HallController.cs
@@ -1,6 +1,7 @@
 ﻿using CineAura.Data.DTO;
 using CineAura.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using System;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
@@ -18,7 +19,6 @@ namespace CineAura.Controllers
         {
             _hallService = hallService;
         }
-
 
         #region GetByMovieId
         [HttpGet("getByMovieId")]
@@ -59,6 +59,84 @@ namespace CineAura.Controllers
         }
         #endregion
 
+        #region GetAll
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var halls = await _hallService.GetAll();
+                return Ok(halls);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Save
+        [HttpPost("save")]
+        public async Task<IActionResult> Save([FromBody] HallDTO request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var saved = await _hallService.Save(request);
+                if (!saved)
+                    return BadRequest("Failed to save hall");
+
+                return Ok(saved);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Update
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(int id, [FromBody] HallDTO request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var result = await _hallService.Update(id, request);
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var result = await _hallService.Delete(id);
+                if (!result)
+                    return NotFound($"Hall with ID {id} not found");
+
+                return Ok(new { success = true, message = "Hall deleted successfully" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
     }
 }
 
