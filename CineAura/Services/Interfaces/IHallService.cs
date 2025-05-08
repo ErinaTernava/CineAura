65c9820afaa83b2630c7b1b03f00f2486ddf5a80
Vesa-Sopjani <vs70037@ubt-uni.net>
2025-05-08 22:36:12 +0200

    added new neccessaery methids in interface for halls

--- CineAura/Services/Interfaces/IHallService.cs
+++ CineAura/Services/Interfaces/IHallService.cs
@@ -8,8 +8,11 @@ namespace CineAura.Services.Interfaces
     public interface IHallService
     {
         Task<List<HallDTO>> GetByMovieId(int movieId);
+        Task<List<HallDTO>> GetAll();
         Task<HallDTO> GetById(int id);
-
+        Task<bool> Save(HallDTO hall);
+        Task<HallDTO> Update(int id, HallDTO hall);
+        Task<bool> Delete(int id);
 
     }
 }
