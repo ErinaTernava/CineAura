ae8b3cdaae0b78b771a2ff3d0731d04df0ff1324
ElionMiftari <ElionMiftari@DESKTOP-UJGH50G>
2025-05-10 23:22:42 +0200

    made slight changes in methods

--- CineAura/Services/Interfaces/IShowtimeService.cs
+++ CineAura/Services/Interfaces/IShowtimeService.cs
@@ -6,10 +6,11 @@ namespace CineAura.Services.Interfaces
 {
     public interface IShowtimeService
 	{
+        Task<List<ShowtimeDisplayDTO>> GetAll();
         Task<ShowtimeDTO> GetById(int id);
         Task<List<ShowtimeDTO>> GetByMovie(int movieId);
 		Task<bool> Save(ShowtimeDTO showtime);
-		Task<bool> Update(int id, ShowtimeDTO showtime);
+		Task<ShowtimeDTO> Update(int id, ShowtimeDTO showtime);
 		Task<bool> Delete(int id);
 	}
 }
