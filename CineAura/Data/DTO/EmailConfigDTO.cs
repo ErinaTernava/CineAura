6da892131db68d69037e9f988e652139c4c42594
ErinaTernava <et70055@ubt-uni.net>
2025-05-12 19:37:30 +0200

    EmailConfigDTO

---
+++ CineAura/Data/DTO/EmailConfigDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CineAura.Data.DTO
+{
+    public class EmailConfigDTO
+    {
+        public int Id { get; set; }
+        public string SenderEmail { get; set; }
+        public string DisplayName { get; set; }
+        public string SmtpHost { get; set; }
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string EncryptedPassword { get; set; }
+    }
+}
