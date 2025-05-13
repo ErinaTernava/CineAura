720cb76eac6ecd49ac10353d1fa4eb9cc311b1e1
ErinaTernava <et70055@ubt-uni.net>
2025-05-13 22:03:22 +0200

    passwordreset dto

---
+++ CineAura/Data/DTO/PasswordResetDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineAura.Data.DTO
+{
+    public class PasswordResetDTO
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+        public bool isUsed { get; set; }
+    }
+}
